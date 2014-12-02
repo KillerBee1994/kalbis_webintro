b04a74c346bfecc7cd99f5c47a9620fb1687d5e3
devisevianus <devisevianus7@gmail.com>
2014-12-02 19:22:56 +0700

    Tugas Flight Booking System

---
+++ Mahasiswa/2014100969_Devis Evianus/Tugas - Flight Booking System/function.js
@@ -0,0 +1,68 @@
+kursi_eco = [];
+kursi_bus = [];
+sisa_kursi_eco = 12;
+sisa_kursi_bus = 8;
+
+function bookingKursi()
+{
+	//ambil nilai kelas
+	kelas = document.getElementById("kelas").value;
+	//ambil nilai jumlah penumpang
+	jml = document.getElementById("jml").value;
+	jml = parseInt(jml);
+	//periksa kelas apa yang dipilih
+
+	//periksa apakah kuota kursi msih mencukupi
+	//jika masih cukup, tambahkan penumpang
+
+if(kelas==2)
+{
+	sisa_kursi_eco -= kursi_eco.length;
+	posisi_kursi = kursi_eco.length;
+
+		if(sisa_kursi_eco>= 1 && jml <= sisa_kursi_eco)
+		{
+			document.getElementById("pesan").innerHTML = "";
+			//tambahkan kursi
+			for(d=1;d<=jml;d++)
+			{
+				next_kursi = posisi_kursi + d + 8;
+				index = next_kursi - 9;
+				document.getElementById(next_kursi).innerHTML = "x";
+				document.getElementById("pesan").innerHTML = "Anda telah memesan kursi Ekonomi, Terima Kasih";
+				kursi_eco[index] = "x";
+			}
+		}
+		else
+		{
+			//kuota kursi tidak mencukupi
+			document.getElementById("pesan").innerHTML = "Kursi Ekonomi tidak mencukupi";
+		}
+}
+
+if(kelas==1)
+{
+	sisa_kursi_bus -= kursi_bus.length;
+	posisi_kursi= kursi_bus.length;
+
+		if(sisa_kursi_bus>= 1 && jml <= sisa_kursi_bus)
+		{
+			document.getElementById("pesan").innerHTML = "";
+			//tambahkan kursi
+			for(d=1;d<=jml;d++)
+			{
+				next_kursi= posisi_kursi + d ;
+				index = next_kursi - 1; 
+				document.getElementById(next_kursi).innerHTML = "x";
+				document.getElementById("pesan").innerHTML = "Anda telah memesan kursi Bisnis, Terima Kasih";
+				kursi_bus[index] = "x";
+			}
+		}
+		else
+		{
+			//kuota kursi tidak mencukupi
+			document.getElementById("pesan").innerHTML = "Kursi Bisnis tidak mencukupi";
+		}
+}
+
+}
